aad376bf743451ee62458bbb116f2fe78bf9b64e
AntonioGibas <antonio.gibas6@gmail.com>
2025-07-23 00:18:55 +0200

    Dodan SQL za insert u tablicu poslovnica

---
+++ Db2/insert/insert_poslovnice.sql
@@ -0,0 +1,9 @@
+-- POSLOVNICE
+INSERT INTO POSLOVNICE VALUES 
+    ('0001', 'Glavna poslovnica Zagreb', 'Ilica 1', 'Zagreb', '10000', '01-1234567', 'A', '2020-01-01');
+
+INSERT INTO POSLOVNICE VALUES 
+    ('0002', 'Poslovnica Split', 'Riva 5', 'Split', '21000', '021-567890', 'A', '2020-01-01');
+
+INSERT INTO POSLOVNICE VALUES 
+    ('0003', 'Poslovnica Rijeka', 'Korzo 10', 'Rijeka', '51000', '051-345678', 'A', '2020-01-01');
