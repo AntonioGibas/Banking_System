5f991083c722998da04f426dfe60c7a2a79320de
AntonioGibas <antonio.gibas6@gmail.com>
2025-07-23 00:21:07 +0200

    Dodan SQL za insert u tablicu rizika ocijena

---
+++ Db2/insert/insert_rizik_ocijena_ref.sql
@@ -0,0 +1,14 @@
+-- RIZIK OCJENE
+
+INSERT INTO RIZIK_OCJENA_REF VALUES 
+    ('N1', 'Nizak rizik - premium', 'Klijenti s izvrsnom kreditnom povijesti', 0.50, 'A');
+INSERT INTO RIZIK_OCJENA_REF VALUES 
+    ('N2', 'Nizak rizik - standard', 'Klijenti s dobrom kreditnom povijesti', 0.75, 'A');
+INSERT INTO RIZIK_OCJENA_REF VALUES 
+    ('S1', 'Srednji rizik - nadziran', 'Klijenti s osrednjom kreditnom povijesti', 1.00, 'A');
+INSERT INTO RIZIK_OCJENA_REF VALUES 
+    ('S2', 'Srednji rizik - povecani', 'Klijenti s problematicnom kreditnom povijesti', 1.25, 'A');
+INSERT INTO RIZIK_OCJENA_REF VALUES 
+    ('V1', 'Visok rizik - ogranicen', 'Klijenti s losim kreditnim rejtingom', 1.50, 'A');
+INSERT INTO RIZIK_OCJENA_REF VALUES 
+    ('V2', 'Visok rizik - maksimalan', 'Klijenti s vrlo losim kreditnim rejtingom', 2.00, 'A');
