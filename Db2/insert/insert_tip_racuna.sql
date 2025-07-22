975a38b9f2d37263baaa2818e081e2b76bb7b30c
AntonioGibas <antonio.gibas6@gmail.com>
2025-07-23 00:23:18 +0200

    Dodan SQL za insert u tablicu tipa racuna

---
+++ Db2/insert/insert_tip_racuna.sql
@@ -0,0 +1,12 @@
+-- TIP RACUNA
+
+INSERT INTO TIP_RACUNA_REF VALUES  
+    ('TEK', 'Tekuci racun', 'Racun za svakodnevne transakcije', 'N', 'D', 'A');
+INSERT INTO TIP_RACUNA_REF VALUES  
+    ('STD', 'Stedni racun', 'Racun za stednju s kamatom', 'D', 'N', 'A');
+INSERT INTO TIP_RACUNA_REF VALUES  
+    ('ORO', 'Oroceni racun', 'Vezana stednja s visokom kamatom', 'D', 'N', 'A');
+INSERT INTO TIP_RACUNA_REF VALUES  
+    ('KRE', 'Kreditni racun', 'Racun za kredite', 'N', 'D', 'A');
+INSERT INTO TIP_RACUNA_REF VALUES  
+    ('KAR', 'Karticni racun', 'Racun vezan za kreditne kartice', 'N', 'D', 'A');
