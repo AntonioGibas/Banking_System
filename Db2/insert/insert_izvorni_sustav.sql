ab89274a8a6162107eadf3d28a78aa09f7dc50b5
AntonioGibas <antonio.gibas6@gmail.com>
2025-07-23 00:29:29 +0200

    Dodan SQL za insert u tablicu izvornog sustava

---
+++ Db2/insert/insert_izvorni_sustav.sql
@@ -0,0 +1,14 @@
+-- IZVORNI SUSTAVI
+
+INSERT INTO IZVORNI_SUSTAV_REF VALUES 
+    ('BAN', 'Bankar salter', 'Transakcija putem bankara na salteru', 5, 'A');
+INSERT INTO IZVORNI_SUSTAV_REF VALUES 
+    ('SLU', 'Sluzba banke', 'Interna sluzba banke', 4, 'A');
+INSERT INTO IZVORNI_SUSTAV_REF VALUES 
+    ('WEB', 'Web banking', 'Online bankarstvo', 3, 'A');
+INSERT INTO IZVORNI_SUSTAV_REF VALUES 
+    ('ACH', 'ACH sustav', 'Automated Clearing House', 3, 'A');
+INSERT INTO IZVORNI_SUSTAV_REF VALUES 
+    ('TEL', 'Telefonsko bankarstvo', 'Bankarstvo putem telefona', 2, 'A');
+INSERT INTO IZVORNI_SUSTAV_REF VALUES 
+    ('LOT', 'Lutrija/ostalo', 'Ostali vanjski sustavi', 1, 'A');
